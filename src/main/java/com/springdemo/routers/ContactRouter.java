8e797182bb1214e66f59d587aa26172c9bba04cc
Weller <weller_pereira@hotmail.com>
2025-01-28 16:23:09 -0300

    Criada a pasta routers, com ContactRouter e tambem a pasta models com Contact

---
+++ src/main/java/com/springdemo/routers/ContactRouter.java
@@ -0,0 +1,13 @@
+package com.springdemo.routers;
+
+import spark.Spark;
+
+public class ContactRouter {
+    public static void routeProcessing(ContactService contactService) {
+        Spark.post("/cadastrar", addContact(contactService));
+        Spark.get("/consultar/:id", getContactId(contactService));
+        Spark.get("/consultar", getAllContacts(contactService));
+        Spark.put("/alterar/:id", updateContact(contactService));
+        Spark.delete("excluir/:id", deleteContact(contactService));
+    }
+}
